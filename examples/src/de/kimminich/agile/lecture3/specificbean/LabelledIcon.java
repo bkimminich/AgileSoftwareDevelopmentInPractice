a34420c1d80926b879615a5c8d1b9133eeeba37e
bkimminich <bjoern.kimminich@gmx.de>
2013-07-30 10:40:42 +0200

    Added LabelledIcon example

---
+++ examples/src/de/kimminich/agile/lecture3/specificbean/LabelledIcon.java
@@ -0,0 +1,24 @@
+package de.kimminich.agile.lecture3.specificbean;
+
+import javax.swing.ImageIcon;
+
+public class LabelledIcon {
+	
+	private ImageIcon image;
+	private String label;
+
+	public LabelledIcon(ImageIcon image, String label) {
+		super();
+		this.image = image;
+		this.label = label;
+	}
+
+	public ImageIcon getImage() {
+		return image;
+	}
+
+	public String getLabel() {
+		return label;
+	}
+
+}
