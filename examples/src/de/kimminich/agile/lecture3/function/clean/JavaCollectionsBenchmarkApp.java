f6a95cb22b7afc2a81ca735a35583e73b8b91d82
bkimminich <bjoern.kimminich@gmx.de>
2013-07-31 10:56:50 +0200

    refactored names, added useful comments, cleaned up code

---
+++ examples/src/de/kimminich/agile/lecture3/function/clean/JavaCollectionsBenchmarkApp.java
@@ -0,0 +1,19 @@
+package de.kimminich.agile.lecture3.function.clean;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.TreeSet;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class JavaCollectionsBenchmarkApp {
+
+    public static void main(String... args) {
+        new ListBenchmark(new ArrayList<String>(), 1000, 10000).execute();
+        new SetBenchmark(new HashSet<String>(), 1000, 10000).execute();
+        new SetBenchmark(new TreeSet<String>(), 1000, 10000).execute();
+        new QueueBenchmark(new LinkedList<String>(), 1000, 10000).execute();
+        new QueueBenchmark(new LinkedBlockingQueue<String>(), 1000, 10000).execute();
+    }
+
+}
