01425332af9ced20b9b6930cd9646ca9c814630d
Björn Kimminich <bjoern.kimminich@gmx.de>
2013-07-31 22:57:09 +0100

    Fixed mixed-up tests

--- examples/src/de/kimminich/agile/lecture7/hamcrest/CombinedMatchersDemoTest.java
+++ examples/src/de/kimminich/agile/lecture7/hamcrest/CombinedMatchersDemoTest.java
@@ -1,38 +1,39 @@
 package de.kimminich.agile.lecture7.hamcrest;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.anyOf;
+import static org.hamcrest.Matchers.both;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.Matchers.hasItem;
-import static org.junit.Assert.assertTrue;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.lessThan;
+import static org.hamcrest.Matchers.not;
 
 import java.util.Arrays;
 import java.util.List;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 public class CombinedMatchersDemoTest {
 
 	@Test
-	@Ignore
-	public void shouldntFindNeedleInHaystack() {
+	public void shouldCombineSimpleMatchers() {
 		// when
-		String[] haystack = { "straw", "straw", "straw", "straw" };
+		int result = 2;
 		// then
-		boolean found = false;
-		for (String straw : haystack) {
-			if (straw.equals("needle")) {
-				found = true;
-			}
-		}
-		assertTrue(found);
+		assertThat(result, is(not(0)));
+		assertThat(result, is(both(greaterThan(1)).and(lessThan(3))));
 	}
 
 	@Test
-	public void shouldFindNeedleInHaystack() {
+	public void shouldCombineCollectionMatchers() {
 		// when
-		List<String> haystack = Arrays.asList("straw", "straw", "straw", "straw");
+		List<Integer> result = Arrays.asList(1, 2);
 		// then
-		assertThat(haystack, hasItem("needle"));
+		assertThat(result, hasItem(anyOf(equalTo(1), equalTo(2))));
+		assertThat(result, hasItem(allOf(equalTo(1), equalTo(2))));
 
 	}
 
