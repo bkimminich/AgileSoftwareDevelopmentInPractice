e44ed4974d22fc101f64cdeac1d3fd01f31e65d6
Björn Kimminich <bjoern.kimminich@gmx.de>
2015-03-09 12:54:50 +0100

    removed curly braces
    (one of those rare cases where it really looks nicer and doesn't hurt to have none)

--- src/de/kimminich/agile/solutions/lecture6/fizzbuzz/FizzBuzz.java
+++ src/de/kimminich/agile/solutions/lecture6/fizzbuzz/FizzBuzz.java
@@ -3,15 +3,10 @@ package de.kimminich.agile.solutions.lecture6.fizzbuzz;
 public class FizzBuzz {
 
     public String process(int number) {
-        if (number % 15 == 0) {
-            return "FizzBuzz";
-        } else if (number % 3 == 0) {
-            return "Fizz";
-        } else if (number % 5 == 0) {
-            return "Buzz";
-        } else {
-            return String.valueOf(number);
-        }
+        if (number % 15 == 0) return "FizzBuzz";
+        else if (number % 3 == 0) return "Fizz";
+        else if (number % 5 == 0) return "Buzz";
+        else return String.valueOf(number);
     }
 
     public static void main(String... args) {
