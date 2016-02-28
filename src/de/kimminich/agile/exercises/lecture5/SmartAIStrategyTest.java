742233cd9d6e187fb7806dd96c88528c217a857e
Bjoern Kimminich <bjoern.kimminich@gmx.de>
2016-02-28 10:50:07 +0100

    implemented smart AI test

--- src/de/kimminich/agile/exercises/lecture5/SmartAIStrategyTest.java
+++ src/de/kimminich/agile/exercises/lecture5/SmartAIStrategyTest.java
@@ -1,17 +1,48 @@
 package de.kimminich.agile.exercises.lecture5;
 
+import de.kimminich.agile.exercises.lecture2.Card;
+import de.kimminich.agile.exercises.lecture2.Player;
+import de.kimminich.agile.exercises.lecture3.ICardChooserStrategy;
 import org.junit.Test;
+
+import static de.kimminich.agile.exercises.lecture5.helper.PlayerHelper.*;
+import static org.junit.Assert.assertEquals;
 
 public class SmartAIStrategyTest {
 
+    ICardChooserStrategy strategy = (player, opponent) -> null; // TODO Instantiate implementation to be tested instead
+
     @Test
     public void shouldNotKeepAnyZeroCardsInHand() {
+        Player player = new Player();
+        putCardsInHandOf(player, 0, 0, 1, 2, 3);
 
+        Player opponent = new Player();
+
+        Card cardToPlay;
+        while ((cardToPlay = strategy.nextCardToPlay(player, opponent)) != null) {
+            simulatePlayingCard(player, cardToPlay);
+        }
+
+        assertEquals(0, countOccurencesOfCardInHand(player, 0));
     }
 
     @Test
     public void shouldPreferToPlayMultipleLowCardsOverSingleHighCardToPreventOverload() {
+        Player player = new Player();
+        player.setMana(7);
+        putCardsInHandOf(player, 1, 2, 4, 7, 6);
 
+        Player opponent = new Player();
+
+        Card cardToPlay;
+        while ((cardToPlay = strategy.nextCardToPlay(player, opponent)) != null) {
+            simulatePlayingCard(player, cardToPlay);
+        }
+
+        assertEquals(1, countOccurencesOfCardInHand(player, 7));
+        assertEquals(1, countOccurencesOfCardInHand(player, 6));
+        assertEquals(0, countTotalOccurencesOfCardsInHand(player, 1, 2, 4));
     }
 
 }
--- src/de/kimminich/agile/exercises/lecture5/helper/PlayerHelper.java
+++ src/de/kimminich/agile/exercises/lecture5/helper/PlayerHelper.java
@@ -3,6 +3,8 @@ package de.kimminich.agile.exercises.lecture5.helper;
 import de.kimminich.agile.exercises.lecture2.Card;
 import de.kimminich.agile.exercises.lecture2.Player;
 
+import java.util.Collections;
+
 public class PlayerHelper {
 
     public static void putCardsInHandOf(Player player, int... cards) {
@@ -15,6 +17,19 @@ public class PlayerHelper {
     public static void simulatePlayingCard(Player player, Card cardToPlay) {
         player.setMana(player.getMana() - cardToPlay.getValue());
         // player.removeCard(card); // TODO
+    }
+
+    public static int countOccurencesOfCardInHand(Player player, int card) {
+        // return Collections.frequency(player.getHand(), new Card(card)); // TODO
+        return -1;
+    }
+
+    public static int countTotalOccurencesOfCardsInHand(Player player, int... cards) {
+        int total = 0;
+        for (int c : cards) {
+            total += countOccurencesOfCardInHand(player, c);
+        }
+        return total;
     }
 
 }
